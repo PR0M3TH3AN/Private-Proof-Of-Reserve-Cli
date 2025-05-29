045e01f87f360948999129db448133bdb41c2f9a
thePR0M3TH3AN <53631862+PR0M3TH3AN@users.noreply.github.com>
2025-05-28 21:14:22 -0400

    Refactor Cargo.toml to define package details and remove workspace section
    Updated Cargo.toml to specify package name, version, and edition. Removed the workspace section to streamline project structure for the private proof of reserve CLI.

---
+++ target/debug/deps/private_proof_of_reserve_cli-3ad53ff63970846f.d
@@ -0,0 +1,5 @@
+/home/user/Documents/GitHub/Private-Proof-Of-Reserve-Cli/target/debug/deps/libprivate_proof_of_reserve_cli-3ad53ff63970846f.rmeta: src/main.rs
+
+/home/user/Documents/GitHub/Private-Proof-Of-Reserve-Cli/target/debug/deps/private_proof_of_reserve_cli-3ad53ff63970846f.d: src/main.rs
+
+src/main.rs:
